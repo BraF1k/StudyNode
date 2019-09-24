126c989fb03c762d2536f3b7a41d92be0e83e511
Serhii Buriak <sergeyburiak44@gmail.com>
2019-09-24 22:46:26 +0300

    Added new files to 'views' folder.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World'))
+// app.set('view engine', 'ejs');
+// app.get('/', (req, res) => res.render('index'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const currency = require("./learnnode/module.js");
+
+console.log(currency);
